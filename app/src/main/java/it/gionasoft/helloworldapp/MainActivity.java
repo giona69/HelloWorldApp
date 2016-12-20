4198ed35f119752d0932c9e2dd11b276dd707f22
giona69 <giona.granata@gmail.com>
2016-12-20 12:16:05 +0100

    Messaggio cambiato

--- app/src/main/java/it/gionasoft/helloworldapp/MainActivity.java
+++ app/src/main/java/it/gionasoft/helloworldapp/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Vengo a pranzo!!!!!!", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Vengo a pranzo!!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
